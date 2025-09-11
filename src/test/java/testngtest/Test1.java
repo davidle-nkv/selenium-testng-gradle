b70bcb67010080485eb44a076da213d13385d53c
davidle-nkv <david.le@nakivo.com>
2025-09-11 14:15:32 +0700

    Try to record video without ffmpeg

--- src/test/java/testngtest/Test1.java
+++ src/test/java/testngtest/Test1.java
@@ -14,7 +14,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.testng.annotations.Listeners;
 
-@Listeners({ VideoListener.class, testngtest.LogListener.class, testngtest.ScreenshotListener.class })
+@Listeners({ VideoListenerWithUI.class, testngtest.LogListener.class, testngtest.ScreenshotListener.class })
 public class Test1 {
     public String baseUrl = "https://www.browserstack.com/";
     public WebDriver driver;    
