961be11867231b53c791e2be89b737ef906561f6
davidle-nkv <david.le@nakivo.com>
2025-09-25 15:01:21 +0700

    Edit the existing file

--- src/test/java/com/nakivo/pages/login/LoginPage.java
+++ src/test/java/com/nakivo/pages/login/LoginPage.java
@@ -3,39 +3,39 @@ package com.nakivo.pages.login;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.time.Duration;
 
 public class LoginPage {
-    private final WebDriver driver;
-    private final WebDriverWait wait;
-
+    private WebDriver driver;
+    private WebDriverWait wait;
+    
     // Locators
-    private final By usernameField = By.id("username");
-    private final By passwordField = By.id("password");
-    private final By loginButton = By.xpath("//button[contains(text(),'Log In')]");
-
+    private By usernameField = By.id("username");
+    private By passwordField = By.id("password");
+    private By loginButton = By.xpath("//button[contains(text(), 'Log In')]");
+    
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
-
+    
     public LoginPage enterUsername(String username) {
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(usernameField));
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(usernameField));
         element.clear();
         element.sendKeys(username);
         return this;
     }
-
+    
     public LoginPage enterPassword(String password) {
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(passwordField));
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(passwordField));
         element.clear();
         element.sendKeys(password);
         return this;
     }
-
-    public void clickLogin() {
+    
+    public void clickLoginButton() {
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(loginButton));
         element.click();
     }
