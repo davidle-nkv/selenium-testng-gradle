b7cb0358d712be8bf7a6c01c924d052e1babf6d1
davidle-nkv <david.le@nakivo.com>
2025-09-26 13:57:59 +0700

    Commit new file generated by N8N

---
+++ src/test/java/com/nakivo/tests/njm1234/replication/ProxmoxReplicationJobTest.java
@@ -0,0 +1,6 @@
+package com.nakivo.tests.njm1234.replication;
+
+import com.nakivo.listeners.LogListener;
+import com.nakivo.listeners.ScreenshotListener;
+import com.nakivo.listeners.VideoListenerWithUI;
+import com.nakivo.pages.njm1234.replication
