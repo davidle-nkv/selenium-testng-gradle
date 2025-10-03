06a04877a701a1392498858d8296cf1246f80675
davidle-nkv <david.le@nakivo.com>
2025-10-03 16:59:48 +0700

    Commit new file generated by N8N

---
+++ src/test/java/com/nakivo/pages/ReIPStepPage.java
@@ -0,0 +1,29 @@
+package com.nakivo.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.time.Duration;
+
+public class ReIPStepPage {
+    private static final int WAIT_TIMEOUT = 15;
+    private WebDriver driver;
+    private WebDriverWait wait;
+    
+    private By nextButton = By.xpath("//button[normalize-space()='Next']");
+
+    public ReIPStepPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT));
+    }
+
+    public void clickNextButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(nextButton));
+        driver.findElement(nextButton).click();
+    }
+
+    public void performReIPStep() {
+        clickNextButton();
+    }
+}
